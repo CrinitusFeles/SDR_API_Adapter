75918171ce9c388884c95d6cf22bb270ef126acf
CrinitusFeles <crinitusfeles@gmail.com>
2024-01-09 20:05:34 +0700

    fixed rx_handler

--- sdr_api_adapter/sdr_model.py
+++ sdr_api_adapter/sdr_model.py
@@ -6,7 +6,6 @@ class SDR_Model:
     def __init__(self, **kwargs) -> None:
         self._kwargs = kwargs
         self.sdr_class = None
-        self.rx_handler = None
         self._running_flag = False
         self.sdr = None
         self.last_msg = None
@@ -14,10 +13,10 @@ class SDR_Model:
     def rx_msg_handler(self, data):
         self.last_msg = data
 
-    def set_rx_handler(self, rx_handler: Callable):
-        self.rx_handler = rx_handler
-        if self.sdr:
-            self.sdr.epy_block_1.rx_handler = rx_handler
+    # def set_rx_handler(self, rx_handler: Callable):
+    #     self.rx_handler = rx_handler
+    #     if self.sdr:
+    #         self.sdr.epy_block_1.rx_handler = rx_handler
 
     def get_config(self):
         return self._kwargs
@@ -34,7 +33,7 @@ class SDR_Model:
             return
         if self.sdr_class:
             self.sdr = self.sdr_class(**self._kwargs)
-            self.sdr.epy_block_1.rx_handler = self.rx_handler
+            self.sdr.epy_block_1.rx_handler = self.rx_msg_handler
             # if self.rx_handler:
             #     self.sdr.rx_msg_handler.subscribe(self.rx_msg_handler)
             #     self.sdr.received.subscribe(self.rx_handler)
@@ -54,7 +53,7 @@ class SDR_Model:
             if not self.sdr_class:
                 return
             self.sdr = self.sdr_class(**self._kwargs)
-            self.sdr.epy_block_1.rx_handler = self.rx_handler
+            self.sdr.epy_block_1.rx_handler = self.rx_msg_handler
             # if self.rx_handler:
             #     self.sdr.rx_msg_handler.subscribe(self.rx_msg_handler)
             #     self.sdr.received.subscribe(self.rx_handler)

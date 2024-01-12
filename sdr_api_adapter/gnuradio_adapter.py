4a454e69461bef4d44964661d9b51d2442f91681
CrinitusFeles <crinitusfeles@gmail.com>
2024-01-12 21:12:50 +0700

    semi-stable fastapi controller

--- sdr_api_adapter/gnuradio_adapter.py
+++ sdr_api_adapter/gnuradio_adapter.py
@@ -1,10 +1,11 @@
 
-from threading import Thread
 import time
 import subprocess
+from threading import Thread
 
 from loguru import logger
 from sdr_api_adapter.zmq_control import ZMQ_Controller
+
 
 
 class GNURadio_Adapter:
@@ -15,7 +16,7 @@ class GNURadio_Adapter:
         self.controller = ZMQ_Controller()
         self.controller.received.subscribe(self.rx_msg_handler)
         self.proc = None
-        self.subprocess_args: list[str] = ['python', 'RX_TX.py']
+        self.subprocess_args: list[str] = ['python', 'examples/RX_TX.py']
         self.proc_thread: Thread
 
     def rx_msg_handler(self, data):
@@ -27,11 +28,17 @@ class GNURadio_Adapter:
     def set_config(self, **kwargs):
         self._kwargs = kwargs
 
-    def send(self, data: bytes | str | list[int]) -> None:
-        self.controller.send_message(data)
+    def send(self, data: bytes | str | list[int], duraion_sec: float = 3) -> None:
+        if self._running_flag:
+            self.set_tx_mode()
+            time.sleep(2)
+            self.controller.send_message(data)
+            time.sleep(duraion_sec)
+            self.set_rx_mode()
+
 
     def routine(self):
-        self.proc = subprocess.Popen(self.subprocess_args, shell=True)
+        self.proc = subprocess.Popen(self.subprocess_args, stdin=subprocess.PIPE)
         # logger.debug(self.proc.stdout.read())  # type: ignore
 
     def start(self, args: list[str] | None = None) -> None:
@@ -39,6 +46,8 @@ class GNURadio_Adapter:
             self.subprocess_args = args
         if self._running_flag:
             return
+        self._running_flag = True
+        self.controller.start()
         logger.debug(f'run with args: {self.subprocess_args}')
         self.proc_thread = Thread(name='proc', target=self.routine, daemon=True)
         self.proc_thread.start()
@@ -47,7 +56,10 @@ class GNURadio_Adapter:
 
     def stop(self) -> None:
         if self._running_flag:
+            self.controller.stop()
+            time.sleep(2)
             self.proc.stdin.close()  # type: ignore
+            self._running_flag = False
 
     def restart(self) -> None:
         if self._running_flag:
@@ -61,5 +73,5 @@ class GNURadio_Adapter:
     def set_rx_mode(self):
         self.controller.set_config({'input_index': 0, 'output_index': 0})
 
+
 gr = GNURadio_Adapter()
-gr.start()
--- sdr_api_adapter/zmq_control.py
+++ sdr_api_adapter/zmq_control.py
@@ -1,13 +1,14 @@
 
 import time
 import socket
-from queue import Queue
+from queue import Empty, Queue
 from threading import Thread
 from gnuradio_pmt.pmt import PMT
 from gnuradio_pmt.zmq_tags import tags, parse_tags
 import zmq
 from zmq.utils.monitor import recv_monitor_message, _MonitorMessage
 from zmq import Socket
+from zmq.error import Again
 from zmq.constants import Event as ZMQ_Event
 from event.event import Event
 from loguru import logger
@@ -29,89 +30,122 @@ def event_monitor(sock: Socket, label: str = ''):
 
 
 class ZMQ_Controller:
-    def __init__(self):
+    def __init__(self) -> None:
+        self.received: Event = Event(dict)
         self.config_port = 5501
         self.msg_port = 5500
         self.client_address = '172.25.111.199:5502'
         self.context = zmq.Context()
-        self.msg_socket: Socket = self.context.socket(zmq.PUSH)
-        self.config_socket: Socket = self.context.socket(zmq.PUSH)
-        self.recv_socket: Socket = self.context.socket(zmq.PULL)
-        self.msg_socket.bind(f"tcp://0.0.0.0:{self.msg_port}")
-        self.config_socket.bind(f"tcp://0.0.0.0:{self.config_port}")
-        self.recv_socket.connect(f"tcp://{self.client_address}")
         self.msg_queue = Queue()
         self.config_queue = Queue()
+        self._running_flag = False
+
+        self.msg_socket: Socket
+        self.config_socket: Socket
+        self.recv_socket: Socket
         self.msg_worker: Thread
         self.config_worker: Thread
         self.recv_worker: Thread
-        self.received: Event = Event(dict)
-        self._running_flag = False
 
-    def send_message(self, data: str | bytes | list[int]):
-        self.msg_queue.put_nowait(data)
+    def send_message(self, data: str | bytes | list[int]) -> None:
+        if self._running_flag:
+            self.msg_queue.put_nowait(data)
 
-    def set_config(self, config: dict):
-        self.config_queue.put_nowait(config)
+    def set_config(self, config: dict | str) -> None:
+        if self._running_flag:
+            self.config_queue.put_nowait(config)
 
     def msg_routine(self) -> None:
         while self._running_flag:
-            msg: str | bytes | list[int] = self.msg_queue.get()
-            tag_data: bytes = tags([(PMT.STRING('packet_len'), PMT.INT32(len(msg)))])
-            if isinstance(msg, str):
-                self.msg_socket.send(tag_data + msg.encode('utf-8'))
-            elif isinstance(msg, bytes):
-                self.msg_socket.send(tag_data + msg)
-            elif isinstance(msg, list):
-                self.msg_socket.send(tag_data + bytes(msg))
-            else:
-                logger.error(f'incorrect msg: {msg}')
+            try:
+                msg: str | bytes | list[int] = self.msg_queue.get(timeout=1)
+                tag_data: bytes = tags([(PMT.STRING('packet_len'), PMT.INT32(len(msg)))])
+                if isinstance(msg, str):
+                    self.msg_socket.send(tag_data + msg.encode('utf-8'))
+                elif isinstance(msg, bytes):
+                    self.msg_socket.send(tag_data + msg)
+                elif isinstance(msg, list):
+                    self.msg_socket.send(tag_data + bytes(msg))
+                else:
+                    logger.error(f'incorrect msg: {msg}')
+            except Empty:
+                pass
+            except Again:
+                logger.error('Timeout to send msg data')
 
     def config_routine(self) -> None:
         while self._running_flag:
-            config: dict = self.config_queue.get()
-            self.config_socket.send(PMT.DICT({PMT.STRING(key): PMT.INT32(val) for key, val in config}).to_bytes())
+            try:
+                config: dict | str = self.config_queue.get(timeout=1)
+                logger.info(f'sending config: {config}')
+                if isinstance(config, dict):
+                    self.config_socket.send(PMT.DICT({PMT.STRING(key): PMT.INT32(val)
+                                                      for key, val in config.items()}).to_bytes())
+                elif isinstance(config, str):
+                    self.config_socket.send(PMT.STRING(config).to_bytes())
+                else:
+                    raise TypeError('incorrect config message type')
+            except Empty:
+                pass
+            except Again:
+                logger.error('Timeout to send config data')
 
     def recv_routine(self) -> None:
         while self._running_flag:
-            msg = self.recv_socket.recv()
             try:
-                src_fd = msg.get(zmq.SRCFD)  # type: ignore
-                logger.debug(f"Socket descriptor of received message: {src_fd}")
-                src_sock = socket.socket(fileno=src_fd)
-                logger.debug(f"Peer address: {src_sock.getpeername()}")
-            except Exception as err:
-                logger.error(err)
-            logger.debug(f'got_msg {len(msg)}: ', msg)
-            tags, payload = parse_tags(msg)
-            self.received.emit({'tags': tags, 'payload': payload})
+                msg: bytes = self.recv_socket.recv()
+            except Again:
+                pass
+            else:
+                try:
+                    tags, payload = parse_tags(msg)
+                    logger.debug(f'parsed data: {tags} {payload}')
+                    self.received.emit({'tags': tags, 'payload': payload})
+                except Exception as err:
+                    logger.error(err)
+                    logger.debug(f'msg has error {len(msg)}: {msg}')
 
-    def start(self):
+    def start(self) -> None:
         if self._running_flag:
             return
         self._running_flag = True
+        self.msg_socket: Socket = self.context.socket(zmq.PUSH)
+        self.config_socket: Socket = self.context.socket(zmq.PUSH)
+        self.recv_socket: Socket = self.context.socket(zmq.PULL)
+        self.recv_socket.setsockopt(zmq.RCVTIMEO, 1000)
+        self.config_socket.setsockopt(zmq.SNDTIMEO, 1000)
+        self.msg_socket.setsockopt(zmq.SNDTIMEO, 1000)
+        self.msg_socket.bind(f"tcp://0.0.0.0:{self.msg_port}")
+        self.config_socket.bind(f"tcp://0.0.0.0:{self.config_port}")
+        self.recv_socket.connect(f"tcp://{self.client_address}")
         self.msg_worker = Thread(name='msg_routine', target=self.msg_routine, daemon=True)
         self.config_worker = Thread(name='config_routine', target=self.config_routine, daemon=True)
         self.recv_worker = Thread(name='recv_routine', target=self.recv_routine, daemon=True)
-        msg_monitor = Thread(target=event_monitor, args=(self.msg_socket, 'MSG monitor:'), daemon=True)
-        config_monitor = Thread(target=event_monitor, args=(self.config_socket, 'CONFIG monitor:'), daemon=True)
-        recv_monitor = Thread(target=event_monitor, args=(self.recv_socket, 'RECV monitor:'), daemon=True)
+        msg_monitor = Thread(name='msg_event_monitor', target=event_monitor,
+                             args=(self.msg_socket, 'MSG monitor:'),  daemon=True)
+        config_monitor = Thread(name='config_event_monitor', target=event_monitor,
+                                args=(self.config_socket, 'CONFIG monitor:'), daemon=True)
+        recv_monitor = Thread(name='recv_event_monitor', target=event_monitor,
+                              args=(self.recv_socket, 'RECV monitor:'), daemon=True)
         self.msg_worker.start()
         self.config_worker.start()
         self.recv_worker.start()
         msg_monitor.start()
         config_monitor.start()
         recv_monitor.start()
+        logger.info('zmq controller started')
 
-    def stop(self):
-        if self._running_flag:
-            self._running_flag = False
-            self.config_socket.close()
-            self.recv_socket.close()
-            self.msg_socket.close()
-            self.msg_worker.join(1)
-            self.config_worker.join(1)
-            self.recv_worker.join(1)
+    def stop(self) -> None:
+        if not self._running_flag:
+            return
+        self._running_flag = False
+        self.msg_worker.join(2)
+        self.config_worker.join(2)
+        self.recv_worker.join(2)
+        self.config_socket.close()
+        self.recv_socket.close()
+        self.msg_socket.close()
+        logger.info('zmq controller stopped')
 
 
 if __name__ == '__main__':
